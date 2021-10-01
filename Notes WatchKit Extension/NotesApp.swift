de365f34db82a85e2193770cdd70b47603e39e1a
Mohd Rozaini <zainirejab@gmail.com>
2021-10-01 18:48:32 +0300

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Bismillah on 1.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
